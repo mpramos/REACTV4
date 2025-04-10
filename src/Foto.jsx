50d3293b2b6c7cdd095e29e15fb6cddc6293c94a
maria <mpramos78915400@gmail.com>
2025-04-09 21:04:55 -0400

    primera estructura en react

---
+++ src/Foto.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Foto = () => {
+  return (
+    <img src="https://rickandmortyapi.com/api/character/avatar/353.jpeg" alt=""  style={{borderRadius:'20px'}}/>
+  )
+}
+
+export default Foto
---
+++ src/Nombre.jsx
@@ -0,0 +1,9 @@
+import React  from "react"
+
+const Nombre=()=>{
+    return (
+    <p>Tiny Rick</p>    
+    
+    )
+}
+export default Nombre
